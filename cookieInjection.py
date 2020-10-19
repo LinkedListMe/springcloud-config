f6449249714138223a84fd2252f0cfa8399404d8
LinkedListMe <61217984+LinkedListMe@users.noreply.github.com>
2020-10-19 22:02:56 +0800

    Create cookieInjection.py

---
+++ cookieInjection.py
@@ -0,0 +1,29 @@
+import json
+class cookieInjection:
+   @classmethod
+   def Injection(self,driver):
+                #cookie绝对路径
+    with open("D:\Python code\myobj\Boss.json", "r") as f:
+        jsonCookies=json.loads(f.read())
+    i=0
+    for cookie in jsonCookies:
+        i = 1
+        if(i<1):
+            driver.add_cookie({
+            'domain': cookie['domain'],
+            'expiry': cookie['expiry'],
+            'httpOnly': cookie['httpOnly'],
+            'name': cookie['name'],
+            'path': cookie['path'],
+            'secure': cookie['secure'],
+            'value': cookie['value']
+            })
+        else:
+            driver.add_cookie({
+                'domain': cookie['domain'],
+                'httpOnly': cookie['httpOnly'],
+                'name': cookie['name'],
+                'path': cookie['path'],
+                'secure': cookie['secure'],
+                'value': cookie['value']
+            })
